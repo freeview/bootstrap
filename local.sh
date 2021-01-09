a9b4cbe2016962fd86910d20da4d36914dc0164d
haskramer <76260818+haskramer@users.noreply.github.com>
2021-01-09 21:15:53 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 BASE_URL_REPO="https://raw.githubusercontent.com/freeview/bootstrap"
-BOOTSTRAP_SCRIPT_MAIN="$BASE_URL_REPO/master/main.sh"
+BRANCH="master"
+BOOTSTRAP_SCRIPT_MAIN="$BASE_URL_REPO/$BRANCH/main.sh"
 
 curl "$BOOTSTRAP_SCRIPT_MAIN" -o "$HOME/main.sh"
 
