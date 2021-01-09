cdf67310691c8dc4c52542a0dd1b812cd25df2b2
bertperrisor <22504259+bertperrisor@users.noreply.github.com>
2021-01-10 00:46:34 +0800

    Save mono version

--- main.sh
+++ main.sh
@@ -10,6 +10,9 @@ git pull
 # Execute wg++
 sh "$LOCAL_EPG_DIR/scripts/epg.sh" > "$LOCAL_EPG_DIR/scripts/epg.log" 2>&1
 
+# Get mono version for reference
+mono --version=number > "$LOCAL_EPG_DIR/scripts/mono-version.txt" 2>&1
+
 # Commit 
 TIMESTAMP=`date`
 cd $LOCAL_EPG_DIR && \
