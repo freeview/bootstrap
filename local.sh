5f3b84064c741a6f719cdda62a948b19e174726d
haskramer <76260818+haskramer@users.noreply.github.com>
2021-01-09 21:09:14 +0800

    Control toggle

---
+++ local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BASE_URL_REPO="https://raw.githubusercontent.com/freeview"
+BOOTSTRAP_SCRIPT_MAIN="$BASE_URL_REPO/bootstrap/master/main.sh"
+
+curl "$BOOTSTRAP_SCRIPT_MAIN" -o "$HOME/main.sh"
+
+# Control toggle main.sh
+sh "$HOME/main.sh"
